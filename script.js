cb9a0e59fa63a8bfc974db03f67f5070fbd18a92
shayes18 <158540532+shayes18@users.noreply.github.com>
2024-02-01 21:09:08 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// Get the modal
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
